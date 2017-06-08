feb2197c6756e208b262bf2ce7ff8b85a420a369
Sergei Chistyakov <slim.akim@gmail.com>
2017-06-09 01:15:44 +0300

    lambdas

---
+++ Lambda/Film.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Lambda
+{
+    public class Film
+    {
+        public string Name { get; set; }
+
+        public int Year { get; set; }
+
+        public Film(string name, int year)
+        {
+            this.Name = name;
+            this.Year = year;
+        }
+    }
+}
---
+++ Lambda/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lambda
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Func<string, int> returnLength;
+
+            //returnLength = delegate (string text) { return text.Length; };
+
+            //returnLength = (string text) => { return text.Length; };
+
+            //returnLength = text => text.Length;
+
+            Action<Film> print =
+                film => Console.WriteLine("Name: {0}, Year: {1}", film.Name, film.Year);
+
+            var films = GetFilms();
+
+            films.ForEach(print);
+
+            films.FindAll(film => film.Year > 1980).ForEach(print);
+
+            films.Sort((f1, f2) => f1.Year.CompareTo(f2.Year));
+
+            films.ForEach(print);
+
+           // Console.WriteLine(returnLength("Hello"));
+        }
+
+        public static List<Film> GetFilms()
+        {
+            var films = new List<Film>
+            {
+                new Film("Jaws", 1981),
+                new Film("Superman", 1977),
+                new Film("Matrix", 1999),
+                new Film("Back to the future", 1985)
+            };
+
+            return films;
+        }
+    }
+}
