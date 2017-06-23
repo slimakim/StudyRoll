2997bcfc8874d70536f1f6feab9ef218303dc0b5
Sergei Chistyakov <slim.akim@gmail.com>
2017-06-24 02:35:19 +0300

    threading

---
+++ ThreadingCore/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadingCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ThreadingFw/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadingFw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(DoSomeWork);
+
+           // thread.IsBackground = true;
+            thread.Start();
+            
+            Thread.Sleep(4000);
+            Console.WriteLine("First thread quitting work");
+
+            
+        }
+
+        public static void DoSomeWork()
+        {
+            while (true)
+            {
+                Console.WriteLine("Second thread working");
+                Thread.Sleep(1500);
+            }
+        }
+
+
+    }
+}
